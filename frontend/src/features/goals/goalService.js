65c5e31be082f247dc157f5918f9efe42096da5e
VihanRajpara <101161470+VihanRajpara@users.noreply.github.com>
2024-02-09 11:42:49 +0530

    Update goalService.js

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/goals/'
+// const API_URL = 'http://localhost:5000/api/goals/'
+const API_URL = 'https://goalsetter-ub7w.onrender.com/api/goals/'
 
 // Create new goal
 const createGoal = async (goalData, token) => {
