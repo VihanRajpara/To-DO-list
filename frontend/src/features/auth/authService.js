a9d171c3decb371b478064957c1089b399c25f75
VihanRajpara <101161470+VihanRajpara@users.noreply.github.com>
2024-02-09 11:42:04 +0530

    Update authService.js

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/users/'
+// const API_URL = 'http://localhost:5000/api/users/'
+const API_URL = 'https://goalsetter-ub7w.onrender.com/api/users/'
 
 // Register user
 const register = async (userData) => {
