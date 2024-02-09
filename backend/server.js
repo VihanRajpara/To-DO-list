1b1054decc3a9b2b8b0dde792f61b2b3a2b2fc1f
VihanRajpara <vihanrajpara3188@gmail.com>
2024-02-09 11:36:56 +0530

    complete project

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000","https://todo-setter.netlify.app"],
   credentials: true, 
   optionSuccessStatus: 200,
 };
